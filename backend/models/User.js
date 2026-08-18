55575cd20ba5659c1658c080382c1db6151ef25b
JAID-KHAN <jaidkhan04072001@gmail.com>
2026-08-18 13:47:06 +0530

    feat(auth): add secure user password hashing

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
 
 const userSchema = new mongoose.Schema(
   {
@@ -16,7 +17,7 @@ const userSchema = new mongoose.Schema(
       trim: true,
     },
 
-    password: {
+    passwordHash: {
       type: String,
       required: true,
     },
@@ -25,5 +26,14 @@ const userSchema = new mongoose.Schema(
     timestamps: true,
   }
 );
+
+// Hash password before saving
+userSchema.pre("save", async function () {
+  if (!this.isModified("passwordHash")) {
+    return;
+  }
+
+  this.passwordHash = await bcrypt.hash(this.passwordHash, 12);
+});
 
 module.exports = mongoose.model("User", userSchema);
